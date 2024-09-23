1636c8a9839b3afbf23d91fc05575a20e9e682c4
yash2163 <yashrajput2163@gmail.com>
2024-09-23 22:45:47 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -76,3 +76,10 @@ window.onscroll = function () {
     navbar.classList.remove("scrolled");
   }
 };
+
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.getElementById("navbar");
+
+hamburger.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
